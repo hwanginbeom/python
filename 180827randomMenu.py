6329433fff140180f86b6476734a3bd6a259d08a
hwanginbeom <rydn2002@gmail.com>
2018-08-27 13:46:31 +0900

    Add files via upload

---
+++ 180827randomMenu.py
@@ -0,0 +1,9 @@
+# 실습2. 점심메뉴추천 챗봇 만들기
+# 0.
+import random
+# 1. 점심메뉴들이 담긴 lunch 배열 만들기
+lunch = ['김밥', '양자강', '육회', '김치찜', '김까']
+# 2. 배열에서 하나 뽑아서 menu 변수에 저장하기
+menu=random.choice(lunch)
+# 3. 원하는 문구로 menu 출력하기 
+print(menu)
